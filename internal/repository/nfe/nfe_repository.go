69568736f8b32c42ac32f6030a22eaf5b3d50101
davi-y08 <oisyzenx@gmail.com>
2025-11-24 19:52:12 -0300

    add get methods for the nfes

--- internal/repository/nfe/nfe_repository.go
+++ internal/repository/nfe/nfe_repository.go
@@ -34,3 +34,29 @@ func (r *NfeRepository) GetById(ctx context.Context, id uint) (*nfe.Nfe, error){
 
 	return &nf, nil
 }
+
+func (r *NfeRepository) GetNFEsByCnpj(ctx context.Context, cnpj string) ([]nfe.Nfe, error){
+	var nfes []nfe.Nfe
+	result := r.db.WithContext(ctx).Where("cnpj = ?", cnpj).Find(&nfes)
+
+	if result.Error != nil{
+		return nil, result.Error
+	}
+
+	return nfes, nil
+}
+
+func (r *NfeRepository) GetAllNFEs(ctx context.Context) ([]nfe.Nfe, error){
+	var nfes []nfe.Nfe
+	result := r.db.WithContext(ctx).Find(&nfes)
+
+	if result.Error != nil{
+		return nil, result.Error
+	}
+
+	return nfes, nil
+}
+
+
+
+//select * from nfes where cnpj = ? 
