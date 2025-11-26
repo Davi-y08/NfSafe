f8cb33cad374eda13f5e3b60dd675d6768ae4f65
davi-y08 <oisyzenx@gmail.com>
2025-11-25 21:02:04 -0300

    create user handler

--- internal/http/handlers/user_handler.go
+++ internal/http/handlers/user_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 	"nf-safe/internal/domain/user"
 	"nf-safe/internal/service"
@@ -25,18 +26,35 @@ func (h *UserHandler) CreateUser(c *gin.Context){
 		})
 		return
 	}
-
-	ctx := c.Request.Context()
 	
-	err := h.userService.CreateUser(ctx, dto.Email, dto.Name, dto.Password)
+	new_user, err := h.userService.CreateUser(c.Request.Context(), dto.Email, dto.Name, dto.Password)
 
 	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error: ": err.Error(),
-		})
+		if errors.Is(err, service.ErrEmailAlreadyExists){
+			c.JSON(http.StatusConflict, gin.H{
+				"error": "email já está em uso",
+			})
+			return
+		}
 
+		if errors.Is(err, service.ErrEmailInvalid) || 
+		errors.Is(err, service.ErrPasswordTooShort) || 
+		errors.Is(err, service.ErrNameTooShort){
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return 
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "erro interno no servidor",
+		})
 		return
 	}
 
-	c.JSON(http.StatusCreated, err)
+	c.JSON(http.StatusCreated, gin.H{
+		"id": new_user.ID,
+		"email": new_user.Email,
+		"name": new_user.Name,
+	})
 }
